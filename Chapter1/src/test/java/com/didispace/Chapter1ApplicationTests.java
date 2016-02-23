5fbbea723a4ab9a98aa19015285fbfb7bb5b284e
程序猿DD <dyc87112@qq.com>
2016-02-23 15:03:39 +0800

    Spring-Boot初始项目

---
+++ Chapter1/src/test/java/com/didispace/Chapter1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.didispace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Chapter1Application.class)
+public class Chapter1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
