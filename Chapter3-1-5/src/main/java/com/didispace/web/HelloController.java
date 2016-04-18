1b11e8a76c0fbdd1ed5c1d32453502b237273a0e
程序猿DD <dyc87112@qq.com>
2016-04-18 15:59:23 +0800

    使用swagger构建restful api

--- Chapter3-1-5/src/main/java/com/didispace/web/HelloController.java
+++ Chapter3-1-5/src/main/java/com/didispace/web/HelloController.java
@@ -3,6 +3,7 @@ package com.didispace.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  *
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @ApiIgnore
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
         return "Hello World";
