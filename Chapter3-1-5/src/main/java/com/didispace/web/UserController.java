eea7b67bb176b7882d654a7247ed58db6fda8161
程序猿DD <dyc87112@qq.com>
2016-04-18 21:09:44 +0800

    使用swagger构建restful api

--- Chapter3-1-5/src/main/java/com/didispace/web/UserController.java
+++ Chapter3-1-5/src/main/java/com/didispace/web/UserController.java
@@ -4,6 +4,9 @@ import com.didispace.domain.User;
 
 import java.util.*;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -19,32 +22,29 @@ public class UserController {
 
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @ApiOperation(value="获取用户列表", notes="")
+    @RequestMapping(value={"/", ""}, method=RequestMethod.GET)
     public List<User> getUserList() {
-        // 处理"/users/"的GET请求，用来获取用户列表
-        // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
         List<User> r = new ArrayList<User>(users.values());
         return r;
     }
 
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public String postUser(@ModelAttribute User user) {
-        // 处理"/users/"的POST请求，用来创建User
-        // 除了@ModelAttribute绑定参数之外，还可以通过@RequestParam从页面中传递参数
+    public String postUser(@RequestBody User user) {
         users.put(user.getId(), user);
         return "success";
     }
 
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
-        // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
-        // url中的id可通过@PathVariable绑定到函数的参数中
         return users.get(id);
     }
 
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
-    public String putUser(@PathVariable Long id, @ModelAttribute User user) {
-        // 处理"/users/{id}"的PUT请求，用来更新User信息
+    public String putUser(@PathVariable Long id, @RequestBody User user) {
         User u = users.get(id);
         u.setName(user.getName());
         u.setAge(user.getAge());
@@ -52,9 +52,9 @@ public class UserController {
         return "success";
     }
 
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public String deleteUser(@PathVariable Long id) {
-        // 处理"/users/{id}"的DELETE请求，用来删除User
         users.remove(id);
         return "success";
     }
