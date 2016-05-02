f45cee396b8990dcc8fe65741d58098cd41c5317
程序猿DD <dyc87112@qq.com>
2016-05-02 10:42:19 +0800

    统一异常处理

---
+++ Chapter3-1-6/src/main/java/com/didispace/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.didispace.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+
+}
