fa5b08a6abab81930309e896d729ea28946e5429
程序猿DD <dyc87112@qq.com>
2016-05-05 14:00:16 +0800

    配置文件详解：自定义属性、随机数、多环境配置等

---
+++ Chapter2-1-1/src/main/java/com/didispace/Application.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
