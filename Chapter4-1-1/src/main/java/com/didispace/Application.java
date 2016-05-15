e8ec3f0ebf69dd65e3b69bc5c104a61652dbb48b
程序猿DD <dyc87112@qq.com>
2016-05-15 10:46:07 +0800

    使用定时任务

---
+++ Chapter4-1-1/src/main/java/com/didispace/Application.java
@@ -0,0 +1,15 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
