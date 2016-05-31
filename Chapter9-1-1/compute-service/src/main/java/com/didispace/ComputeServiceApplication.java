7da6df80613a29d6d7cd7e2ebef145ccea606682
程序猿DD <dyc87112@qq.com>
2016-05-31 21:56:19 +0800

    Spring Cloud构建微服务架构（一）——服务注册与发现

--- Chapter9-1-1/compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
+++ Chapter9-1-1/compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class ComputeServiceApplication {
 
 	public static void main(String[] args) {
