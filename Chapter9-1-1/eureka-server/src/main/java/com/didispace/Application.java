469798e649507c73d517bbe64dc13e0932934b85
程序猿DD <dyc87112@qq.com>
2016-05-31 21:38:15 +0800

    Spring Cloud构建微服务架构（一）——服务注册与发现

--- Chapter9-1-1/eureka-server/src/main/java/com/didispace/Application.java
+++ Chapter9-1-1/eureka-server/src/main/java/com/didispace/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
