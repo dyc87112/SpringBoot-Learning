febab47c56414fd96f7fdf0ced3fe69c7ab2400f
程序猿DD <dyc87112@qq.com>
2016-06-02 13:06:04 +0800

    Spring Cloud构建微服务架构（二）——服务消费者

---
+++ Chapter9-1-2/eureka-ribbon/src/main/java/com/didispace/web/ConsumerController.java
@@ -0,0 +1,21 @@
+package com.didispace.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    }
+
+
+}
