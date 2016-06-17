c363372833a5d09836de5234d2d3e08f65e5a40d
程序猿DD <dyc87112@qq.com>
2016-06-17 12:30:52 +0800

    缓存支持：注解配置与EhCache使用

---
+++ Chapter4-4-1/src/main/java/com/didispace/domain/UserRepository.java
@@ -0,0 +1,22 @@
+package com.didispace.domain;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @date 16/3/23 下午2:34.
+ * @blog http://blog.didispace.com
+ */
+@CacheConfig(cacheNames = "users")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Cacheable(key = "#p0", condition = "#p0.length() < 10")
+    User findByName(String name);
+
+}
