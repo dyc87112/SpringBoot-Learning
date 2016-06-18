4aac8ea89989bfcc5d535d7174e076fbd028e3dd
程序猿DD <dyc87112@qq.com>
2016-06-18 23:30:43 +0800

    使用Redis做集中式缓存

---
+++ Chapter4-4-2/src/main/java/com/didispace/domain/UserRepository.java
@@ -0,0 +1,24 @@
+package com.didispace.domain;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @date 16/3/23 下午2:34.
+ * @blog http://blog.didispace.com
+ */
+@CacheConfig(cacheNames = "users")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Cacheable(key = "#p0")
+    User findByName(String name);
+
+    @CachePut(key = "#p0.name")
+    User save(User user);
+
+}
