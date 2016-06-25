1fc884ec9253aa44d7697dbcd636caa50bd6d3cd
zhaiyongchao <didi@potatomato.club>
2016-06-25 22:23:49 +0800

    Spring Cloud构建微服务架构（三）断路器

---
+++ Chapter9-1-3/eureka-ribbon/src/main/java/com/didispace/web/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.didispace.web;
+
+import com.didispace.service.ComputeService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private ComputeService computeService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return computeService.addService();
+    }
+
+}
