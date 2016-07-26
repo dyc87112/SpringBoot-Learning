1069c79832591392b94647ed60fd16da769f565c
程序猿DD <dyc87112@qq.com>
2016-07-26 23:33:06 +0800

    Spring Cloud构建微服务架构（五）服务网关

--- Chapter9-1-5/api-gateway/src/main/java/com/didispace/Application.java
+++ Chapter9-1-5/api-gateway/src/main/java/com/didispace/Application.java
@@ -1,8 +1,10 @@
 package com.didispace;
 
+import com.didispace.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -10,6 +12,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 
 }
---
+++ Chapter9-1-5/api-gateway/src/main/java/com/didispace/filter/AccessFilter.java
@@ -0,0 +1,47 @@
+package com.didispace.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AccessFilter extends ZuulFilter  {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+            log.warn("access token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token ok");
+        return null;
+    }
+
+}
