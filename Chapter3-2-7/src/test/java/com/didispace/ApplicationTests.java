ae68a4750672994b9a66ec216ce173563f251e1d
程序猿DD <dyc87112@qq.com>
2016-08-19 21:37:50 +0800

    整合MyBatis

--- Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@Transactional
 public class ApplicationTests {
 
 	@Autowired
