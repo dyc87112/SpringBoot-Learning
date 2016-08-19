1c355c3ffe31bb235be52f81e426a787a7e22c4e
翟永超 <dyc87112@qq.com>
2016-08-19 23:11:20 +0800

    MyBatis使用详解

---
+++ Chapter3-2-8/src/main/java/com/didispace/domain/User.java
@@ -0,0 +1,47 @@
+package com.didispace.domain;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
---
+++ Chapter3-2-8/src/main/java/com/didispace/domain/UserMapper.java
@@ -0,0 +1,36 @@
+package com.didispace.domain;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM user WHERE name = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Results({
+            @Result(property = "name", column = "name"),
+            @Result(property = "age", column = "age")
+    })
+    @Select("SELECT name, age FROM user")
+    List<User> findAll();
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+
+    @Update("UPDATE user SET age=#{age} WHERE name=#{name}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name}, #{age})")
+    int insertByUser(User user);
+
+    @Insert("INSERT INTO user(name, age) VALUES(#{name,jdbcType=VARCHAR}, #{age,jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+}
