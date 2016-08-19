af21e4f9f1047a0b2bb5f8b2bcd964cacd74b8c1
翟永超 <didi@potatomato.club>
2016-08-19 21:33:38 +0800

    整合MyBatis

--- Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@Transactional
 public class ApplicationTests {
 
 	@Autowired
