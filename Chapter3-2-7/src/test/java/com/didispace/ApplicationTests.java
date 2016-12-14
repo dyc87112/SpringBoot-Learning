cfe10f8b8f1359a7de054a84171b07ada2c43358
翟永超 <dyc87112@qq.com>
2016-12-14 14:02:18 +0800

    整合MyBatis

--- Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter3-2-7/src/test/java/com/didispace/ApplicationTests.java
@@ -3,13 +3,13 @@ package com.didispace;
 import com.didispace.domain.User;
 import com.didispace.domain.UserMapper;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
