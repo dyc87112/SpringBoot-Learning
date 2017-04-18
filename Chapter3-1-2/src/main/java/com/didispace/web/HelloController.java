0825c1942bc3797067893cdd31f66b84bf5b4158
zongzhankui <zongzhankui@gomeholdings.com>
2017-04-18 15:58:28 +0800

    ResponseBody没有import

--- Chapter3-1-2/src/main/java/com/didispace/web/HelloController.java
+++ Chapter3-1-2/src/main/java/com/didispace/web/HelloController.java
@@ -3,7 +3,7 @@ package com.didispace.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
