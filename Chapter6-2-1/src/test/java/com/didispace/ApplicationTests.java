f9c3aa9e938ccb94ededfb4669d8ae63c04db9ff
didi <dyc87112@qq.com>
2017-06-02 00:30:58 +0800

    /info端点输出Git版本信息

---
+++ Chapter6-2-1/src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void test1() throws Exception {
+
+	}
+
+}
