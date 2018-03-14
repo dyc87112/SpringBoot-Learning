abc8dfdea7f4998f4ffceaa454f34e8e5eeb4b57
zhaiyongchao <dyc87112@qq.com>
2018-03-14 08:46:28 +0800

    Spring Boot以及Feign使用Java 8中LocalDate等时间日期类的问题解决

--- Chapter3-1-7/src/main/java/com/didispace/Application.java
+++ Chapter3-1-7/src/main/java/com/didispace/Application.java
@@ -1,15 +1,19 @@
 package com.didispace;
 
-import com.didispace.dto.UserDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 /**
  * @author 程序猿DD
@@ -30,6 +34,16 @@ public class Application {
         public UserDto user(@RequestBody UserDto userDto) throws Exception {
             return userDto;
         }
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class UserDto {
+
+        private String userName;
+        private LocalDate birthday;
 
     }
 
