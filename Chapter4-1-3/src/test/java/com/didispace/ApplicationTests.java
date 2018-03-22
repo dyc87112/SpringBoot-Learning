fd635d96b35c0cdd2e58f7ba3b7e321f31eb44d3
zhaiyongchao <dyc87112@qq.com>
2018-03-22 16:37:01 +0800

    使用@Async实现异步调用：自定义线程池

---
+++ Chapter4-1-3/src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,28 @@
+package com.didispace;
+
+import com.didispace.async.Task;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    @Autowired
+    private Task task;
+
+    @Test
+    public void test() throws Exception {
+
+        task.doTaskOne();
+        task.doTaskTwo();
+        task.doTaskThree();
+
+        Thread.currentThread().join();
+    }
+
+}
