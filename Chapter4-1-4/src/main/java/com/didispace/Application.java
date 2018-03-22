4bf616ec0c9e5b056b9fb136bf107d1766a45402
zhaiyongchao <dyc87112@qq.com>
2018-03-22 17:16:09 +0800

    使用@Async实现异步调用：资源优雅关闭

---
+++ Chapter4-1-4/src/main/java/com/didispace/Application.java
@@ -0,0 +1,36 @@
+package com.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.Executor;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @EnableAsync
+    @Configuration
+    class TaskPoolConfig {
+
+        @Bean("taskExecutor")
+        public Executor taskExecutor() {
+            ThreadPoolTaskScheduler executor = new ThreadPoolTaskScheduler();
+            executor.setPoolSize(20);
+            executor.setThreadNamePrefix("taskExecutor-");
+
+            executor.setWaitForTasksToCompleteOnShutdown(true);
+            executor.setAwaitTerminationSeconds(60);
+            return executor;
+        }
+
+    }
+
+}
