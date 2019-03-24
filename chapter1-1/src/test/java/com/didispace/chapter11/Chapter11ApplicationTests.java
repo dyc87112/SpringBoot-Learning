59b86f4519f8559bb8c1de3ec1aa9b5ec9b7e936
zhaiyongchao <dyc87112@qq.com>
2019-03-24 21:59:08 +0800

    创建基础项目

---
+++ chapter1-1/src/test/java/com/didispace/chapter11/Chapter11ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.didispace.chapter11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter11ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
