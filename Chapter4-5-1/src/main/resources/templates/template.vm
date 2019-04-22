f771c85877495a80b33641f145b5424b012b9303
zhaiyongchao <dyc87112@qq.com>
2019-04-22 11:46:15 +0800

    清理master下的代码例子，原来的内容分别根据spring boot的版本做迁移，1.x内容移到1.x分支，2.x内容移到2.x分支

--- Chapter4-5-1/src/main/resources/templates/template.vm
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-    <h3>你好， ${username}, 这是一封模板邮件!</h3>
-</body>
-</html>
--- Chapter6-1-1/src/main/java/com/didispace/States.java
+++
@@ -1,7 +0,0 @@
-package com.didispace;
-
-public enum States {
-    UNPAID,                 // 待支付
-    WAITING_FOR_RECEIVE,    // 待收货
-    DONE                    // 结束
-}
