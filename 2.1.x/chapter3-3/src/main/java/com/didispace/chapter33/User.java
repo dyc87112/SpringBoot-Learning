9f712442f0cf3ec0f45b912b0b57dc024f72c85b
zhaiyongchao <dyc87112@qq.com>
2020-02-08 19:59:54 +0800

    Spring Boot 2.x基础教程：使用国产数据库连接池Druid

---
+++ 2.1.x/chapter3-3/src/main/java/com/didispace/chapter33/User.java
@@ -0,0 +1,13 @@
+package com.didispace.chapter33;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private Integer age;
+
+}
