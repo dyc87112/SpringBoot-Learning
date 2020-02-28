b5b48e24c825fba175d92e048f95e6543c403893
zhaiyongchao <dyc87112@qq.com>
2020-02-28 09:54:44 +0800

    Spring Boot 2.x基础教程：使用MyBatis访问MySQL

--- 2.1.x/chapter3-5/src/main/java/com/didispace/chapter35/Chapter35Application.java
+++ 2.1.x/chapter3-5/src/main/java/com/didispace/chapter35/Chapter35Application.java
@@ -2,8 +2,6 @@ package com.didispace.chapter35;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Chapter35Application {
--- 2.1.x/chapter3-5/src/test/java/com/didispace/chapter35/Chapter35ApplicationTests.java
+++ 2.1.x/chapter3-5/src/test/java/com/didispace/chapter35/Chapter35ApplicationTests.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.util.List;
 
-@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class Chapter35ApplicationTests {
 
     @Autowired
