94ededc59d2ef67346226a81391b08848e853603
翟永超 <dyc87112@qq.com>
2021-03-03 17:50:30 +0800

    Spring Boot 2.x基础教程：快速入门

--- 2.x/chapter1-1/src/test/java/com/didispace/chapter11/Chapter11ApplicationTests.java
+++ 2.x/chapter1-1/src/test/java/com/didispace/chapter11/Chapter11ApplicationTests.java
@@ -2,10 +2,8 @@ package com.didispace.chapter11;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Chapter11ApplicationTests {
 
