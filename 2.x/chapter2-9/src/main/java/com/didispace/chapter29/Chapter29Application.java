aa4a2881f0b5c28e8b9226f3b0b9800c6611adef
翟永超 <dyc87112@qq.com>
2021-07-20 15:48:17 +0800

    Spring Boot 2.x基础教程：使用Elastic Job实现定时任务

---
+++ 2.x/chapter2-9/src/main/java/com/didispace/chapter29/Chapter29Application.java
@@ -0,0 +1,18 @@
+package com.didispace.chapter29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ */
+@SpringBootApplication
+public class Chapter29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter29Application.class, args);
+    }
+
+}
