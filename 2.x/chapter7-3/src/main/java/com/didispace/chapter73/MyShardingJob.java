1fe9c48c771fb0564d0d5b5aa533f979a45f1c80
翟永超 <dyc87112@qq.com>
2021-07-21 17:53:44 +0800

    Spring Boot 2.x基础教程：使用Elastic Job的分片配置

--- 2.x/chapter7-3/src/main/java/com/didispace/chapter73/MyShardingJob.java
+++ 2.x/chapter7-3/src/main/java/com/didispace/chapter73/MyShardingJob.java
@@ -11,15 +11,16 @@ public class MyShardingJob implements SimpleJob {
 
     @Override
     public void execute(ShardingContext context) {
+        // sharding-total-count=3，所以任务被分为三个分片
         switch (context.getShardingItem()) {
             case 0:
-                log.info("do something by sharding item 0");
+                log.info("分片1：执行任务");
                 break;
             case 1:
-                log.info("do something by sharding item 1");
+                log.info("分片2：执行任务");
                 break;
             case 2:
-                log.info("do something by sharding item 2");
+                log.info("分片3：执行任务");
                 break;
         }
     }
