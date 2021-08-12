e12aa4f076d5029fb18e7aeefb580b3961174619
翟永超 <dyc87112@qq.com>
2021-08-12 17:18:44 +0800

    update

---
+++ 2.x/chapter1-5/src/main/java/com/didispace/chapter15/Chapter15Application.java
@@ -0,0 +1,13 @@
+package com.didispace.chapter15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter15Application.class, args);
+	}
+
+}
