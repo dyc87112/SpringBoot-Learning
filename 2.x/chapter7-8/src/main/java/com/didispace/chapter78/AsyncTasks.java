ddcb8cc33230cf7465f31f4d347ca2b870703714
程序猿DD <dyc87112@qq.com>
2021-09-22 17:50:49 +0800

    为@Async异步任务线程池配置拒绝策略

--- 2.x/chapter7-8/src/main/java/com/didispace/chapter78/AsyncTasks.java
+++ 2.x/chapter7-8/src/main/java/com/didispace/chapter78/AsyncTasks.java
@@ -23,14 +23,4 @@ public class AsyncTasks {
         return CompletableFuture.completedFuture("任务完成");
     }
 
-    @Async("taskExecutor2")
-    public CompletableFuture<String> doTaskTwo(String taskNo) throws Exception {
-        log.info("开始任务：{}", taskNo);
-        long start = System.currentTimeMillis();
-        Thread.sleep(random.nextInt(10000));
-        long end = System.currentTimeMillis();
-        log.info("完成任务：{}，耗时：{} 毫秒", taskNo, end - start);
-        return CompletableFuture.completedFuture("任务完成");
-    }
-
 }
--- 2.x/chapter7-8/src/main/java/com/didispace/chapter78/Chapter78Application.java
+++ 2.x/chapter7-8/src/main/java/com/didispace/chapter78/Chapter78Application.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
@@ -35,8 +36,28 @@ public class Chapter78Application {
             executor.setKeepAliveSeconds(60);
             executor.setThreadNamePrefix("executor-1-");
 
-            // 配置拒绝策略
-            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+            /**配置拒绝策略**/
+
+            // AbortPolicy策略：默认策略，如果线程池队列满了丢掉这个任务并且抛出RejectedExecutionException异常。
+//            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+
+            // DiscardPolicy策略：如果线程池队列满了，会直接丢掉这个任务并且不会有任何异常。
+//            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+
+            // DiscardOldestPolicy策略：如果队列满了，会将最早进入队列的任务删掉腾出空间，再尝试加入队列。
+//            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());
+
+            // CallerRunsPolicy策略：如果添加到线程池失败，那么主线程会自己去执行该任务，不会等待线程池中的线程去执行。
+//            executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+            // 自定义策略
+//            executor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+//                @Override
+//                public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+//
+//                }
+//            });
+
             return executor;
         }
 
