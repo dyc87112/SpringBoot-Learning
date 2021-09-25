2c71a23d0c76f9b49f7d486b2c6254534c619feb
mengld <mengld@hotmail.com>
2021-09-25 17:19:30 +0800

    Chapter1-4的HelloController包名错误

--- 2.x/chapter1-4/src/main/java/com/didispace/chapter14/HelloController.java
+++ 2.x/chapter1-4/src/main/java/com/didispace/chapter14/HelloController.java
@@ -1,4 +1,4 @@
-package com.didispace.chapter11;
+package com.didispace.chapter14;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
