ead166d5caa12ba6351ae3b489ba2e29daf88801
Administrator <1269301496@qq.com>
2022-09-15 14:13:10 +0800

    创建practice1

---
+++ hs/prac1/src/main/java/com/example/prac1/Prac1Application.java
@@ -0,0 +1,19 @@
+package com.example.prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 黄帅
+ * @Description //TODO
+ * @Date 14:11 2022/9/15
+ * @Param
+ **/
+@SpringBootApplication
+public class Prac1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac1Application.class, args);
+    }
+
+}
---
+++ hs/prac1/src/test/java/com/example/prac1/Prac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
